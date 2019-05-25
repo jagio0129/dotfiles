837e3d63b5db764e5ddf04e452701d6f8211e883
jagio0129 <jagio0129@gmail.com>
2019-05-25 17:36:15 +0900

    mac向けのnotification機能の追加とプロンプト色付け

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -4,3 +4,5 @@ ln -s ~/dotfiles/.vim ~/.vim
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 cat .gitconfig >> ../.gitconfig
 echo 'alias be="bundle exec"' >> ~/.bashrc
+echo "export PS1='\[\e[0;35m\][\h@\w]\$\[\e[0;0m\]'" >> ~/.bashrc
+echo "alias noti='terminal-notifier -message 'コマンド完了''" >> ~/.bashrc
