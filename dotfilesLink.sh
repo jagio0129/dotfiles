b520ef586d57833dffa583426ef3acd62db65470
jagio0129 <jagio0129@gmail.com>
2019-06-15 20:29:49 +0900

    add alias docker-compose

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -4,5 +4,6 @@ ln -s ~/dotfiles/.vim ~/.vim
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 cat .gitconfig >> ../.gitconfig
 echo 'alias be="bundle exec"' >> ~/.bashrc
+echo 'alias dc="docker-compse"' >> ~/.bashrc
 echo "export PS1='\[\e[0;35m\][\h@\w]\$\[\e[0;0m\]'" >> ~/.bashrc
-echo "alias noti='terminal-notifier -message 'コマンド完了''" >> ~/.bashrc
+echo "alias noti='terminal-notifier -message 'コマンド完了''" >> ~/.bashrc # for mac
