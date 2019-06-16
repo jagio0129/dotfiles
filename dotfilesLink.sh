741825f285b5749e65f217c1351cb6a12aa6ca87
jagio0129 <jagio0129@gmail.com>
2019-06-16 22:35:09 +0900

    login時にtmuxを自動起動する設定を追加

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -3,7 +3,20 @@ ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.vim ~/.vim
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 cat .gitconfig >> ../.gitconfig
+
 echo 'alias be="bundle exec"' >> ~/.bashrc
 echo 'alias dc="docker-compse"' >> ~/.bashrc
 echo "export PS1='\[\e[0;35m\][\h@\w]\$\[\e[0;0m\]'" >> ~/.bashrc
 echo "alias noti='terminal-notifier -message 'コマンド完了''" >> ~/.bashrc # for mac
+
+# tmux auto start when login
+SESSION_NAME=ope
+if [[ -z "$TMUX" && -z "$STY" ]] && type tmux >/dev/null 2>&1; then
+  option=""
+  if tmux has-session -t ${SESSION_NAME}; then
+    option="attach -t ${SESSION_NAME}"
+  else
+    option="new -s ${SESSION_NAME}"
+  fi
+  tmux $option && exit
+fi
