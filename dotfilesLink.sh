34d2d88e61446f248802b8e7ca153ec0a1d8f2b9
jagio0129 <jagio0129@gmail.com>
2019-06-19 22:16:24 +0900

    絶対パスを使うように変更

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -2,7 +2,7 @@
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.vim ~/.vim
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
-cat .gitconfig >> ../.gitconfig
+cat .gitconfig >> ~/.gitconfig
 
 echo 'alias be="bundle exec"' >> ~/.bashrc
 echo 'alias dc="docker-compse"' >> ~/.bashrc
